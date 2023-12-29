43c3ff403cb9c3d90285e7699fe9a4b1ad328938
Martin Blomberg <martin.blomberg@outlook.com>
2023-12-29 13:35:02 +0100

    Add jooq example

---
+++ jooq/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.example.jooq.test.tables.City;
+import org.example.jooq.test.tables.Country;
+import org.example.jooq.test.tables.records.CityRecord;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+//        String xmlString = Files.readString(Path.of(Main.class.getResource("/jooq-config.xml").toURI()));
+//        GenerationTool.generate(xmlString);
+
+        String userName = "root";
+        String password = "password";
+        String url = "jdbc:mysql://localhost:3306/test";
+        Connection conn = DriverManager.getConnection(url, userName, password);
+
+        DSLContext context = DSL.using(conn, SQLDialect.MYSQL);
+
+        var country = context.fetchOne(Country.COUNTRY, Country.COUNTRY.COUNTRY_NAME.eq("Sverige"));
+
+        CityRecord city = context.newRecord(City.CITY);
+        city.setCityName("Malmö");
+        city.setInhabitants(357_377);
+        city.setCountryId(country.getCountryId());
+
+        city.store();
+
+    }
+}
