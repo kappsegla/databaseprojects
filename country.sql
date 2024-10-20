c35c517640387f5591efefc624fcbbe84b538257
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-20 20:11:33 +0200

    Add sql file to create database

---
+++ country.sql
@@ -0,0 +1,16 @@
+create database if not exists test;
+use test;
+drop table if exists country;
+create table country
+(
+    country_id    int          not null
+        primary key,
+    country_name  varchar(255) null,
+    language_code varchar(255) null
+);
+
+INSERT INTO country (country_id, country_name, language_code)
+VALUES (1, 'Sweden', 'sv'),
+       (2, 'Norway', 'no'),
+       (3, 'Denmark', 'da'),
+       (4, 'Germany', 'de');
